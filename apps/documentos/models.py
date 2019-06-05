7bf5201a23571e2ff38a31003ee8f9b4010765a1
sauloemagna <sauloqueiroz2@gmail.com>
2019-06-05 13:51:13 -0300

    criando relacionamento de DB

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from apps.funcionarios.models import Funcionario
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100)
+    titular = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.descricao
