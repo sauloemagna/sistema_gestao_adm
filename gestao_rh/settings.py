969b007926c4b64b19a6ebc9205916087ee69456
sauloemagna <sauloqueiroz2@gmail.com>
2019-06-08 10:07:33 -0300

    pasta static

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -137,4 +137,4 @@ LOGOUT_REDIRECT_URL = 'login'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
